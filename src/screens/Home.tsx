ba6e61bea4fff9f02c1f245743f70ac33cf4a48c
Lucas Rodrigues dos Santos <45694838+llucasy@users.noreply.github.com>
2024-12-03 14:29:12 +0000

    wip: habitday component

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,9 +1,24 @@
-import { View } from "react-native";
+import { View, Text } from "react-native";
+
+const weekDays = ['D', 'S', 'T', 'Q', 'Q', 'S', 'S']
+
+import { HabitDay, DAY_SIZE } from "../components/HabitDay";
+import { Header } from "../components/Header";
 
 export function Home() {
-    return (
-        <View className="flex-1">
+  return (
+    <View className="flex-1 bg-background px-8 pt-16">
+      <Header />
 
-        </View>
-    )
+      <View>
+        {weekDays.map((weekDay, i) => (
+          <Text>
+            {weekDay}
+          </Text>
+        ))}
+      </View>
+
+      <HabitDay />
+    </View>
+  )
 }
